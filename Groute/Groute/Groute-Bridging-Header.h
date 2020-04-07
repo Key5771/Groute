fa8bd5c4eccaee183e45e1f0102aed710f54c3e8
Key5771 <ksj57715@hanmail.net>
2020-04-07 17:49:56 +0900

    KakaoMap 불러오기 완료 Firebase 설정 완료

---
+++ Groute/Groute/Groute-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Groute-Bridge-Header.h
+//  Groute
+//
+//  Created by 김기현 on 2020/04/07.
+//  Copyright © 2020 김기현. All rights reserved.
+//
+
+#ifndef Groute_Bridge_Header_h
+#define Groute_Bridge_Header_h
+
+#import <DaumMap/MTMapView.h>
+
+#endif /* Groute_Bridge_Header_h */
---
+++ Groute/Groute/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Groute
+//
+//  Created by 김기현 on 2020/04/07.
+//  Copyright © 2020 김기현. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, MTMapViewDelegate {
+
+    var mapView: MTMapView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView = MTMapView(frame: self.view.frame)
+        
+        if let mapView = mapView {
+            mapView.delegate = self
+            mapView.baseMapType = .standard
+            self.view.addSubview(mapView)
+        }
+    }
+}
+
