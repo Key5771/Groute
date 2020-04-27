1b668fc080de02f510103d6c209cc4b17b245892
Key5771 <ksj57715@hanmail.net>
2020-04-27 18:10:03 +0900

    Login 기능 구현 완료

--- Groute/Groute/LoginViewController.swift
+++ Groute/Groute/LoginViewController.swift
@@ -12,6 +12,9 @@ import GoogleSignIn
 
 class LoginViewController: UIViewController {
     @IBOutlet weak var googleLogin: GIDSignInButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var idTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +26,24 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func loginButtonClick(_ sender: Any) {
+        Auth.auth().signIn(withEmail: idTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            if user != nil {
+                let viewController: UIViewController = self.storyboard!.instantiateViewController(withIdentifier: "navigation")
+                viewController.modalPresentationStyle = .overFullScreen
+                self.present(viewController, animated: true, completion: nil)
+            } else {
+                let alertController = UIAlertController(title: "로그인 실패", message: "아이디 또는 패스워드를 확인하여주세요.", preferredStyle: .alert)
+                let okButton = UIAlertAction(title: "확인", style: .default, handler: nil)
+                
+                alertController.addAction(okButton)
+                self.present(alertController, animated: true, completion: nil)
+                
+                self.idTextfield.text = ""
+                self.passwordTextfield.text = ""
+            }
+        }
+    }
     /*
     // MARK: - Navigation
 
